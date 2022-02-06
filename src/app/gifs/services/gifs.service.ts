e560ea45cb86ee35ad40f17b85d1bb649fb1b16b
Arley Rivas G <arrivasg@unipacifico.edu.co>
2022-02-06 17:56:50 -0500

    Agregando y leyendo datos del localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,7 +17,16 @@ export class GifsService {
     return [...this._historial]; // [...] rompemos la relacion con el arreglo original
   }
 
-  constructor(private http: HttpClient) { }
+  //los constructores de los services solo se ejecutaran una sola vez en toda la app
+  constructor(private http: HttpClient) { 
+    //sacamos el historial del localStorage si es vacio, pasamos un []
+    this._historial = JSON.parse(localStorage.getItem('historial')!)|| [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    // if(localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')! );
+    // }
+
+  }
 
   buscarGifs(queryBusqueda: string = '') {
     //quitamos espacios y pasamos a minisculas antes de almacenar
@@ -26,9 +35,12 @@ export class GifsService {
     // y miramos si no esta incluido en la lista
     if (!this._historial.includes(queryBusqueda)) {
       this._historial.unshift(queryBusqueda);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
     if (this._historial.length > 9) {
       this._historial = this._historial.slice(0, 10); //.splice corta el arreglo 
+
+      
     }
 
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=MKMQbJfpEAupRowipGUqnVFicN1Q1yDd&q=dragon ball&limit=10')
@@ -42,6 +54,7 @@ export class GifsService {
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
 
     //el suscribe se ejecuta cuando ya tenemos una respuesta de la peticion
